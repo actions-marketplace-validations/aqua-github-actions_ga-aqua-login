d3c9567ea938f0dd95786f38fb1780b6d3058b74
Constantin Krüger <Constantinidis@web.de>
2021-05-26 12:35:01 +0200

    update to new method login

--- action.js
+++ action.js
@@ -1,18 +1,19 @@
-const Aqua = require('./lib/common/Aqua')
+const Aqua = require('./lib/common/Aqua');
 
 module.exports = class {
-    constructor({argv}) {
-        this.Aqua = new Aqua({
-            baseUrl: argv.baseUrl,
-            username: argv.username,
-            password: argv.password
-        })
+  constructor({argv}) {
+    this.Aqua = new Aqua({
+      baseUrl: argv.baseUrl,
+    });
 
-        this.argv = argv
-    }
+    this.argv = argv;
+  }
 
-    async execute () {
-        const token = await this.Aqua.login()
-        return token
-    }
-}
+  async execute() {
+    const token = await this.Aqua.login({
+      username: this.argv.username,
+      password: this.argv.password,
+    });
+    return token;
+  }
+};
--- index.js
+++ index.js
@@ -1,34 +1,33 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 
-const Action = require('./action')
+const Action = require('./action');
 
-async function exec () {
+async function exec() {
   try {
-    if (!(process.env.AQUA_BASE_URL || core.getInput('aqua-base-url'))) throw new Error('Please specify aqua-base-url as input or AQUA_BASE_URL as env')
-    if (!(process.env.AQUA_PASSWORD || core.getInput('aqua-password'))) throw new Error('Please specify aqua-password as input or AQUA_PASSWORD as env')
-    if (!(process.env.AQUA_USERNAME || core.getInput('aqua-username'))) throw new Error('Please specify aqua-username as input or AQUA_USERNAME as env')
+    /* eslint max-len: "off" */
+    if (!(process.env.AQUA_BASE_URL || core.getInput('aqua-base-url'))) throw new Error('Please specify aqua-base-url as input or AQUA_BASE_URL as env');
+    if (!(process.env.AQUA_PASSWORD || core.getInput('aqua-password'))) throw new Error('Please specify aqua-password as input or AQUA_PASSWORD as env');
+    if (!(process.env.AQUA_USERNAME || core.getInput('aqua-username'))) throw new Error('Please specify aqua-username as input or AQUA_USERNAME as env');
 
     const argv = {
-        baseUrl: process.env.AQUA_BASE_URL || core.getInput('aqua-base-url'),
-        username: process.env.AQUA_USERNAME || core.getInput('aqua-username'),
-        password: process.env.AQUA_PASSWORD || core.getInput('aqua-password')
-    }
+      baseUrl: process.env.AQUA_BASE_URL || core.getInput('aqua-base-url'),
+      username: process.env.AQUA_USERNAME || core.getInput('aqua-username'),
+      password: process.env.AQUA_PASSWORD || core.getInput('aqua-password'),
+    };
 
-    const result = await new Action({argv}).execute()
+    const result = await new Action({argv}).execute();
     if (result) {
-        core.setOutput('token', result['access_token'])
-        core.setSecret(result['access_token'])
-        return
+      core.setOutput('token', result['access_token']);
+      core.setSecret(result['access_token']);
+      return;
     }
 
-    console.log('Failed to login.')
-    process.exit(78)
-
+    console.log('Failed to login.');
+    process.exit(78);
   } catch (error) {
-    console.error(error)
-    process.exit(1)
+    console.error(error);
+    process.exit(1);
   }
 }
 
-exec()
+exec();
